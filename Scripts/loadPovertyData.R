53bedf608e20c3ea2b2fbfbd61bced282feb45bd
AJ <ajamontesa@gmail.com>
2023-04-08 11:02:14 +0800

    Monthly inflation update - March 2023

--- Scripts/loadPovertyData.R
+++ Scripts/loadPovertyData.R
@@ -10,3 +10,9 @@ writeLines("Loading Poverty Data into R.")
 poverty <- read_xlsx("Data/Poverty/Poverty.xlsx") %>%
     pivot_longer(cols = -(1:3), names_to = "Year", values_to = "Value") %>%
     mutate(Year = parse_date(Year, "%Y"))
+
+
+poverty_sectors <- read_xlsx("Data/Poverty/Poverty.xlsx", sheet = "Basic Sectors") %>%
+    pivot_longer(cols = -(Indicator:Series), names_to = "Year", values_to = "Value") %>%
+    mutate(Year = parse_date(Year, "%Y"))
+
